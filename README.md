8411b085f36d663f94ec1f5d99bc826658546903
Hebilicious <xsh4k3@gmail.com>
2023-06-11 06:32:28 +0700

    feat: migrate to nuxt3

--- README.md
+++ README.md
@@ -1,74 +1,42 @@
-# emmanuel-style
+# Nuxt 3 Minimal Starter
 
-Emmanuel's landing page.
+Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
-## Build Setup
+## Setup
 
-```bash
-# install dependencies
-$ yarn install
-
-# serve with hot reload at localhost:3000
-$ yarn dev
-
-# build for production and launch server
-$ yarn build
-$ yarn start
-
-# generate static project
-$ yarn generate
-```
-
-### Themes
+Make sure to install the dependencies:
 
 ```bash
-black: #000000;
-darktheme: #1B1C31;
-grey: #C6C6C6;
-beige: #FDFAF5;
-darkbeige: #FEF1E0;
-golden: #FFE5C4;
-lightpink: #F4BED8;
-vividpink: #FF8DD5;
-darkpink: #FF7699;
-ultramarine: #7887FE;
-lightblue: #21BFFE;
-lightgreen: #91D7D9;
-green: #6DD5BE;
+# yarn
+yarn install
 
-*profile box shadow*
-box-shadow: -10px 15px 30px 0 rgba(0,0,0,0.13);
+# npm
+npm install
 
-*article box shadow*
-box-shadow: -1px -1px 30px 0 rgba(136, 136, 136, 0.11), -1px, -1px, 30px, 0 rgba(49,49,49,0.9);
-height: 75px;
-width: 280px;
-
-*linear gradient*
-background: linear-gradient(28deg, #FEF1E0 0%, #F4BED8 50%, #B2F2E4 100%);
+# pnpm
+pnpm install
 ```
 
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+## Development Server
 
-```js
-const meta = [
-    // Twitter Card
-    { name: "twitter:card", content: "summary" },
-    { name: "twitter:title", content: "Vue Social Cards Example" },
-    { name: "twitter:description", content: "Vue sample site showing off Twitter and Facebook Cards." },
-    // image must be an absolute path
-    { name: "twitter:image", content: this.logo },
-    // Facebook OpenGraph
-    { property: "og:title", content: "Vue Social Cards Example" },
-    { property: "og:site_name", content: "Vue Example" },
-    { property: "og:type", content: "website" },
-    { property: "og:image", content: this.logo },
-    { property: "og:description", content: "Vue sample site showing off Twitter and Facebook Cards." }
-]
+Start the development server on `http://localhost:3000`
+
+```bash
+npm run dev
 ```
 
-Safari specific things :
+## Production
 
--   bug with vh and 100% childrens
--   doesn't support webp images
--   doesn't support flex gap
+Build the application for production:
+
+```bash
+npm run build
+```
+
+Locally preview production build:
+
+```bash
+npm run preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
--- pages/index.vue
+++ pages/index.vue
@@ -1,118 +1,85 @@
+<script lang="ts" setup>
+import type { Theme } from "~/composables/useTheme"
+defineOptions({ name: "Index" })
+
+const iconHeight = "1.5rem"
+const { mode, themesList, rootRef } = useTheme()
+
+// Rainbow text
+const SideName = computed(() => {
+    return { SideName: true, PaperSideName: themesList.get(mode.value as Theme) === "PaperTheme" }
+})
+</script>
 <template>
-    <div v-show="isReady" id="rootId" ref="rootRef" :class="['Root', currentTheme]">
-        <div class="Top">
-            <div class="PageContainer">
-                <div class="Landing">
-                    <div class="ContentWrapper">
-                        <div class="Content">
-                            <div class="Heading">
-                                <picture>
-                                    <source
-                                        :srcSet="require('~/assets/Profile.png?format=webp&size=100')"
-                                        type="image/webp"
-                                    />
-                                    <source
-                                        :srcSet="require('~/assets/Profile.png?resize&size=100')"
-                                        type="image/png"
-                                    />
-                                    <img alt="profilePicture" :src="require('~/assets/Profile.png?resize&size=100')" />
-                                </picture>
-                            </div>
-                            <div class="Name">
-                                <h2>Emmanuel LD</h2>
-                            </div>
-                            <div class="Logos">
-                                <div class="Icons">
-                                    <ExternalLink label="twitter" url="twitter.com/its_hebilicious">
-                                        <Twitter :icon-height="iconHeight" />
-                                    </ExternalLink>
-                                    <ExternalLink label="github" url="github.com/Hebilicious">
-                                        <Github :icon-height="iconHeight" />
-                                    </ExternalLink>
-                                    <ExternalLink label="linkedIn" url="www.linkedin.com/in/emmanuel-donnet">
-                                        <LinkedIn :icon-height="iconHeight" />
-                                    </ExternalLink>
+    <div>
+        <div id="rootId" ref="rootRef" class="Root">
+            <div class="Top">
+                <div class="PageContainer">
+                    <div class="Landing">
+                        <div class="ContentWrapper">
+                            <div class="Content">
+                                <div class="Heading">
+                                    <nuxt-picture src="/Profile.png" width="100" height="100" provider="imagekit" />
+                                </div>
+                                <div class="Name">
+                                    <h2>Emmanuel LD</h2>
+                                </div>
+                                <div class="Logos">
+                                    <div class="Icons">
+                                        <ExternalLink label="twitter" url="twitter.com/its_hebilicious">
+                                            <Twitter :icon-height="iconHeight" />
+                                        </ExternalLink>
+                                        <ExternalLink label="github" url="github.com/Hebilicious">
+                                            <Github :icon-height="iconHeight" />
+                                        </ExternalLink>
+                                        <ExternalLink label="linkedIn" url="www.linkedin.com/in/emmanuel-donnet">
+                                            <LinkedIn :icon-height="iconHeight" />
+                                        </ExternalLink>
+                                    </div>
+                                </div>
+                                <div class="MainText">
+                                    <div>I live in London.</div>
+                                    <div>I do computer things.</div>
+                                </div>
+                                <div class="SubText">
+                                    The noblest pleasure is the joy of understanding.
+                                    <br />
+                                    - DaVinci
                                 </div>
                             </div>
-                            <div class="MainText">
-                                <div>
-                                    I live in London.
-                                </div>
-                                <div>
-                                    I do computer things.
-                                </div>
-                            </div>
-                            <div class="SubText">
-                                The noblest pleasure is the joy of understanding.
-                                <br />
-                                - DaVinci
+                            <div :class="SideName">
+                                <h1>
+                                    <span>E</span><span>m</span><span>m</span><span>a</span><span>n</span><span>u</span
+                                    ><span>e</span><span>l</span>
+                                </h1>
                             </div>
                         </div>
-                        <div :class="SideName">
-                            <h1>
-                                <span>E</span><span>m</span><span>m</span><span>a</span><span>n</span><span>u</span
-                                ><span>e</span><span>l</span>
-                            </h1>
+                        <div class="Separation">
+                            <div></div>
+                            <div class="FakeLine"></div>
                         </div>
-                    </div>
-                    <div class="Separation">
-                        <div></div>
-                        <div class="FakeLine"></div>
                     </div>
                 </div>
+                <!-- <div class="PageContainer"></div> -->
+                <!-- <div class="PageContainer"></div> -->
+                <!-- <div class="PageContainer"></div> -->
+                <!-- <div class="PageContainer"></div> -->
             </div>
-            <!-- <div class="PageContainer"></div> -->
-            <!-- <div class="PageContainer"></div> -->
-            <!-- <div class="PageContainer"></div> -->
-            <!-- <div class="PageContainer"></div> -->
-        </div>
-        <div class="Bottom">
-            <div class="ThemeSwitcher">
-                <ThemeButton v-for="theme in themesList" :key="theme[0]" :theme="theme" />
+            <div class="Bottom">
+                <ClientOnly>
+                    <div class="ThemeSwitcher">
+                        <ThemeButton v-for="theme in themesList" :key="theme[0]" :theme="theme" />
+                    </div>
+                </ClientOnly>
             </div>
         </div>
     </div>
 </template>
 
-<script lang="ts">
-import { useTheme } from "@/services/useTheme"
-import { defineComponent, watch, ref, onBeforeMount, computed } from "nuxt-composition-api"
-
-export default defineComponent({
-    name: "Index",
-    setup() {
-        // @ts-ignore
-        const rootRef = ref<HTMLElement>(null)
-        const isReady = ref(false)
-        const { currentTheme, themesList, selectTheme, setNavbarColor } = useTheme()
-        // Change the color of mobile navbar
-        watch(currentTheme, () => {
-            if (rootRef.value === null) return
-            const color = getComputedStyle(rootRef.value).getPropertyValue("--navbarColor")
-            setNavbarColor(color)
-        })
-        // Rainbow text
-        const SideName = computed(() => {
-            return { SideName: true, PaperSideName: currentTheme.value === "PaperTheme" }
-        })
-
-        onBeforeMount(() => {
-            const saved = window.localStorage.getItem("theme")
-            const { themeName, color } = saved ? JSON.parse(saved) : { themeName: "dark", color: "#1b1c31" }
-            selectTheme(themeName)
-            setNavbarColor(color)
-            isReady.value = true
-        })
-        // dirty set default theme color
-        return { currentTheme, themesList, selectTheme, rootRef, isReady, SideName, iconHeight: "1.5rem" }
-    }
-})
-</script>
-
 <style lang="postcss">
 .Root {
     /* Force landscape hack */
-    @screen mLandscape {
+    @media (--mLandscape) {
         height: 100vmax !important;
     }
 
@@ -223,7 +190,7 @@ export default defineComponent({
 
 .Content {
     @apply grid gap-4 pt-24;
-    @screen mLandscape {
+    @media (--mLandscape) {
         @apply pt-8;
     }
 }
